c99fe328ef2030b7c8c0cb9ace97715651cdbd45
Urteusz <mateusz.urbaniak.23@gmail.com>
2025-03-23 15:32:26 +0100

    Finished BFS alorithm

--- algorithm.py
+++ algorithm.py
@@ -1,9 +1,18 @@
+import time
+
 import numpy as np
 from collections import deque
 
 from scipy.optimize import newton
 
 EMPTY_TILE = 0
+
+directions = {
+            'L': (0, -1),  # Lewo
+            'R': (0, 1),   # Prawo
+            'U': (-1, 0),  # Góra
+            'D': (1, 0)    # Dół
+        }
 
 def is_solvable(tiles):
     inv_count = 0
@@ -35,7 +44,7 @@ def get_neighbours(i, j, width, height):
   if i > 0: moves.append((i - 1, j))
   if i < height - 1: moves.append((i + 1, j))
   if j > 0: moves.append((i, j - 1))
-  if j < width - 1: moves.append(i, j + 1)
+  if j < width - 1: moves.append((i, j + 1))
   return moves
 
 
@@ -53,3 +62,88 @@ def swap(puzzle, i1, j1, i2, j2):
 
 def puzzle_to_tuple(puzzle):
     return tuple(tuple(row) for row in puzzle)
+
+
+def bfs(puzzle, search_order):
+    start_time = time.time()
+    height, width = puzzle.shape
+
+    # Konwersja do krotki dla hashowania
+    initial_state = puzzle_to_tuple(puzzle)
+
+    # Kolejka dla BFS
+    queue = deque([initial_state])
+
+    # Słownik do śledzenia odwiedzonych stanów
+    visited = {initial_state: True}
+
+    # Słownik do śledzenia ścieżki (poprzedników i wykonanych ruchów)
+    parent = {initial_state: None}
+    move_direction = {initial_state: None}
+
+    # Stan docelowy
+    target = matrix(width, height, list(range(1, width * height)) + [0])
+    target = puzzle_to_tuple(target)
+
+    # Liczniki do statystyk
+    visited_states = 1
+    processed_states = 0
+    max_depth = 0
+
+    while queue:
+        current_state = queue.popleft()
+        processed_states += 1
+
+        # Konwersja z krotki z powrotem na tablicę
+        current_puzzle = np.array(current_state)
+
+        # Sprawdź, czy osiągnięto stan docelowy
+        if current_state == target:
+            # Odtwarzanie ścieżki
+            path = []
+            state = current_state
+            while move_direction[state] is not None:
+                path.append(move_direction[state])
+                state = parent[state]
+            path.reverse()
+
+            # Określenie głębokości rozwiązania
+            solution_depth = len(path)
+            max_depth = max(max_depth, solution_depth)
+
+            end_time = time.time()
+            execution_time = (end_time - start_time) * 1000
+            return path, visited_states, processed_states, max_depth, execution_time
+
+        # Znajdź pozycję pustego pola
+        i, j = find_zero(current_puzzle)
+
+        # Sprawdź wszystkie możliwe ruchy zgodnie z podanym porządkiem
+        for direction in search_order:
+            di, dj = directions[direction]
+            ni, nj = i + di, j + dj
+
+            # Sprawdź, czy ruch jest możliwy
+            if 0 <= ni < height and 0 <= nj < width:
+                # Wykonaj ruch
+                new_puzzle = swap(current_puzzle, i, j, ni, nj)
+                new_state = puzzle_to_tuple(new_puzzle)
+
+                # Jeśli ten stan nie był jeszcze odwiedzony
+                if new_state not in visited:
+                    queue.append(new_state)
+                    visited[new_state] = True
+                    visited_states += 1
+                    parent[new_state] = current_state
+                    move_direction[new_state] = direction
+
+                    # Aktualizacja maksymalnej głębokości
+                    current_depth = 0
+                    state = current_state
+                    while state != initial_state:
+                        current_depth += 1
+                        state = parent[state]
+                    max_depth = max(max_depth, current_depth + 1)
+
+    # Jeśli nie znaleziono rozwiązania
+    return None, visited_states, processed_states, max_depth
--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import random
 import os
 from os.path import basename
 import re
-
 from algorithm import *
+
 
 SIZE_HEIGHT = 4
 SIZE_WIDTH = 4
@@ -13,21 +13,10 @@ def read_board(filename):
     with open(filename, 'r') as file:
         lines = file.readlines()
 
-        SIZE_HEIGHT, SIZE_WIDTH = map(int, lines[0].split())
+    SIZE_HEIGHT, SIZE_WIDTH = map(int, lines[0].split())
+    puzzle = np.array([list(map(int, line.split())) for line in lines[1:]])
 
-        puzzle = [list(map(int, line.split())) for line in lines [1:]]
-
-        return puzzle, SIZE_WIDTH, SIZE_HEIGHT
-#
-# def shuffle_board():
-#     tiles = list(range(SIZE_HEIGHT * SIZE_WIDTH))
-#     while True:
-#         random.shuffle(tiles)
-#         if is_solvable(tiles):
-#             return tiles
-
-
-
+    return puzzle  # Teraz zwraca od razu numpy array
 
 
 def print_board(tiles):
@@ -36,34 +25,13 @@ def print_board(tiles):
             print(tiles[SIZE_HEIGHT * i + j], end=' ')
 
 
-# def move_tile(tiles, tile):
-#     if tile not in tiles or tile == EMPTY_TILE:
-#         return tiles
-#
-#     index = tiles.index(tile)
-#     empty_index = tiles.index(EMPTY_TILE)
-#     row, col = divmod(index, SIZE_WIDTH)
-#     empty_row, empty_col = divmod(empty_index, SIZE_WIDTH)
-#
-#     if abs(row - empty_row) + abs(col - empty_col) == 1:
-#         tiles[index], tiles[empty_index] = tiles[empty_index], tiles[index]
-#     return tiles
-
-def solve(acronym, parametr, file_shuffled, file_solved, file_addons):
-    if acronym == "bfs":
-        print(parametr)
-    elif acronym == "dfs":
-        print(parametr)
-    elif acronym == "astr":
-        print(parametr)
-
 def save_solved(solved, file_name):
     dlugosc_rozw = len(solved)
-    plik = open('file_name','w')
+    plik = open(file_name,'w')
     if dlugosc_rozw == 1 and solved == -1:
         plik.write(solved + '\n')
     else:
-        plik.write(dlugosc_rozw + '\n')
+        plik.write(str(dlugosc_rozw) + '\n')
         for i in range(len(solved)):
             if i == dlugosc_rozw-1:
                 plik.write(str(solved[i]) + '\n')
@@ -71,21 +39,41 @@ def save_solved(solved, file_name):
                 plik.write(str(solved[i]) + ' ')
     plik.close()
 
-def save_addons(solved, file_addons, file_name):
+def save_addons(solved, file_name, visited, processed, max_depth, time):
     dlugosc_rozw = len(solved)
-    plik = open('file_name','w')
+    plik = open(file_name,'w')
     if dlugosc_rozw == 1 and solved == -1:
         plik.write(solved + '\n')
+        plik.close()
     else:
-        plik.write(dlugosc_rozw + '\n')
+        plik.write(str(dlugosc_rozw) + '\n')
+        plik.write(str(visited) + '\n')
+        plik.write(str(processed) + '\n')
+        plik.write(str(max_depth) + '\n')
+        plik.write(str(time))
 
-        print("to be continued...")
+
+def solve(acronym, parametr, file_shuffled, file_solved, file_addons):
+    puzzle = read_board(file_shuffled)
+    if acronym == "bfs":
+        path, visited_states, processed_states, max_depth, timer = bfs(puzzle, parametr)
+        save_solved(path,file_solved)
+        save_addons(path, file_addons, visited_states, processed_states, max_depth, timer)
+    elif acronym == "dfs":
+        print(parametr)
+    elif acronym == "astr":
+        print(parametr)
 
 
 def main():
+    acronym = input()
+    parametr = input()
+    file_shuffled = input()
+    file_solved = input()
+    file_addons = input()
+    solve(acronym, parametr, file_shuffled, file_solved, file_addons)
 
-    tiles = shuffle_board()
-    print_board(tiles)
+
 
 
 
